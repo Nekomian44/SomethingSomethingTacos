74b3cb5d4fce40bc8c964f407912999b75763378
atocco <alex.tocco88@gmail.com>
2017-11-08 12:25:31 -0500

    Main Menu
    adding a mainn menu to the game with a skybox / sounds / and animations. There are a couple to-dos that i will put in an issue for this

---
+++ EnemySpawnTest/Assets/Scripts/mouseHover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseHover : MonoBehaviour {
+
+	MeshRenderer renderer;
+	public bool isStart;
+	public bool isExit;
+	public AudioClip msover;
+	public AudioClip start;
+	public AudioClip exit;
+	AudioSource aud;
+
+	void Start(){
+		renderer = this.GetComponent<MeshRenderer> ();
+		aud = this.GetComponent<AudioSource> ();
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseEnter(){
+		aud.clip = msover;
+		aud.Play ();
+		renderer.material.color = Color.red;
+	}
+
+	void OnMouseExit() {
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseUp(){
+		if(isStart)
+		{
+			aud.clip = start;
+			aud.Play ();
+			// TODO insert scene name as string in the parenthises
+			Application.LoadLevel (0);
+		}
+		if (isExit)
+		{
+			aud.clip = exit;
+			aud.Play ();
+			Application.Quit();
+		}
+	}
+}
